e372157380ab25fa2fdee78119dd823a4f90c650
lucas-travassos <lucasstravassos19@gmail.com>
2026-03-25 21:46:31 -0300

    Iniciado conexão com banco de dados

--- CRUD-MVC-POO-PHP/configuration/connect.php
+++ CRUD-MVC-POO-PHP/configuration/connect.php
@@ -1,2 +1,32 @@
 <!-- Conexão com Banco de Dados -->
 
+<?php
+    // definção de constantes
+    define('HOST', 'localhost');
+    define('DATABASENAME', 'crud-mvcc-poo-php-lm');
+    define('USER', 'root');
+    define('PASSWORD', 'root');
+
+    class Connect{
+        protected $connection;
+
+        function __construct()
+        {
+            $this->connectDatabase();
+        }
+
+        function connectDatabase()
+        {
+            try
+            {
+                $this->connection = new PDO('mysql:host='.HOST.';dbname='.DATABASENAME, USER, PASSWORD);
+            }
+            catch (PDOException $e)
+            {
+                echo "Error!".$e->getMessage();
+                die();
+            }
+        }
+    }
+    $testConnection = new Connect();
+?>

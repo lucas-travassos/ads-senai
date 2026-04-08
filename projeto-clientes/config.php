ef2edc25b74e61af1c3804c0e0279aa1ff57d28f
lucas-travassos <lucasstravassos19@gmail.com>
2026-04-08 20:36:02 -0300

    Renomeado diretório e configurado para funcionar localmente

--- projeto-clientes/config.php
+++ projeto-clientes/config.php
@@ -2,7 +2,7 @@
 include_once('include/Database.php');
 define('SS_DB_NAME', 'cliente');//banco da dados
 define('SS_DB_USER', 'root');//usuario banco
-define('SS_DB_PASSWORD', 'root');///senha
+define('SS_DB_PASSWORD', '');///senha
 define('SS_DB_HOST', 'localhost');//servidor
 
 $dsn	= 	"mysql:dbname=".SS_DB_NAME.";host=".SS_DB_HOST."";

41e8844a898af552729c913307e7e6e93d81033e
lucas-travassos <lucasstravassos19@gmail.com>
2026-04-10 23:41:40 -0300

    Excluído projeto duplicado e padronizado nome das pastas no modelo kebab-case

--- CRUD-AULA-UCHOA/form.php
+++
@@ -1,7 +0,0 @@
-<h2>Cadastro de usuários</h2>
-
-<form action="salvar.php" method="POST">
-    Nome: <input type="text" name="nome"><br>
-    Email: <input type="text" name="email"><br>
-    <button>Salvar</button>
-</form>
